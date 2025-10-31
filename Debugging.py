f671bb38b026196ebabf9ebb2ce015288a4623b3
Blaze <darshanchougule821@gmail.com>
2025-10-31 23:17:21 +0530

    Removed unwanted files from Git tracking only

--- Debugging.py
+++
@@ -1,9 +0,0 @@
-"""Fix Errors"""
-try:
-    num=int(input("Enter a number : "))
-except ValueError:
-    print("you have typed an invalid value,please try again with a numerical number")
-    num = int(input("Enter a number : "))
-
-if num>18:
-    print("You are eligible to ride a bike")
